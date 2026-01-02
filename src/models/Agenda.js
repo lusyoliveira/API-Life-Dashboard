931d93bba6e1837eb5f50af54e82f272b3a3056a
lusyoliveira <lusyoliveira@outlook.com>
2026-01-02 13:37:57 -0300

    Corrigido a importação da agenda; Corrigido o model agenda para referencia ao model status;

--- src/models/Agenda.js
+++ src/models/Agenda.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
  
 const agendaSchema = new mongoose.Schema({
     Titulo: { type: String, required: true },
-    Status: {type: mongoose.Schema.Types.ObjectId, ref: 'agendastatus', required: true},
+    Status: {type: mongoose.Schema.Types.ObjectId, ref: 'status', required: true},
     Categoria: {type: mongoose.Schema.Types.ObjectId, ref: 'agendacategorias', required: true},
     Tipo: {type: mongoose.Schema.Types.ObjectId, ref: 'agendatipos', required: true},
     Data: { type: String, required: true }
