4f1b76c8d2c446f34d4c7b740b187ce4c43902d6
lusyoliveira <lusyoliveira@outlook.com>
2026-01-30 15:48:00 -0300

    Retirada a obrigatóriedade do campo categoria em transações;

--- src/models/Transacao.js
+++ src/models/Transacao.js
@@ -4,7 +4,7 @@ const transacaoSchema = new mongoose.Schema({
    
     Descricao: { type: String, required: [true, "Descrição é obrigatória"] },
     Data: { type: String, required: [true, "Data é obrigatória"] },
-    Categoria: {type: mongoose.Schema.Types.ObjectId, ref: 'categorias', required: [true, "Categoria é obrigatória"]},
+    Categoria: {type: mongoose.Schema.Types.ObjectId, ref: 'categorias'},
     ContaOrigem: { type:  mongoose.Schema.Types.ObjectId, ref: 'conta', required: [true, "Conta origem é obrigatória"] },
     ContaDestino: { type:  mongoose.Schema.Types.ObjectId, ref: 'conta' },
     Valor: { type: Number, required: [true, "Valor é obrigatório"] },
