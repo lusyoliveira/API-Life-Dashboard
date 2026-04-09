d64cdb666adc7329b3c8fa038ce81b6e00aaeaaf
Luciene Oliveira <Luciene Oliveira@CASA2>
2026-04-09 17:29:43 -0300

    Alterada a server js para usar dns do mongo

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+import dns from 'node:dns';
 import "dotenv/config";
-import app from "./src/app.js"
+
+// Configura o DNS primeiro!
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const PORT = 3000;
 
-app.listen(PORT, () =>{
-    console.log("Servidor Iniciado");    
-});
+// Agora importa o app somente após o DNS estar pronto
+async function start() {
+    const { default: app } = await import("./src/app.js");
+    
+    app.listen(PORT, () => {
+        console.log("Servidor Iniciado com DNS customizado");
+    });
+}
+
+start();
 
 //subir servidor npm run dev
 //localhost:3000/usuarios
