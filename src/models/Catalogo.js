d453446795a2b0e313c0a0e8fb26d8f3034db53d
Luciene <lusyoliveira@outlook.com>
2026-09-01 10:41:35 -0300

    Adicionado novos campos ao modelo Catalogo: original_name, overview, poster_path, media_type, genres_ids, popularity, first_air_date, year e vote_average. para integração com TMDB

--- src/models/Catalogo.js
+++ src/models/Catalogo.js
@@ -18,6 +18,16 @@ const Catalogo = sequelize.define("Catalogo", {
     adicao: { type: DataTypes.DATE, allowNull: false },
     dias: { type: DataTypes.INTEGER, allowNull: true },
     progresso: { type: DataTypes.INTEGER, allowNull: true },
+    id_tmdb: { type: DataTypes.INTEGER, allowNull: true },
+    original_name: { type: DataTypes.STRING, allowNull: true },
+    overview: { type: DataTypes.TEXT, allowNull: true },
+    poster_path: { type: DataTypes.BLOB, allowNull: true },
+    media_type: { type: DataTypes.STRING, allowNull: true },
+    genres_ids: { type: DataTypes.JSON, allowNull: true },
+    popularity: { type: DataTypes.FLOAT, allowNull: true },
+    first_air_date: { type: DataTypes.DATE, allowNull: true },
+    year: { type: DataTypes.INTEGER, allowNull: true },
+    vote_average: { type: DataTypes.FLOAT, allowNull: true },
 }, { 
     tableName: "catalogos",
     timestamps: false
